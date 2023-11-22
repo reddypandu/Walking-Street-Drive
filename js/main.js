9031814c961d93eaa79f39cbc79367b517612d8a
reddypandu <128275448+reddypandu@users.noreply.github.com>
2023-11-22 23:27:22 +0530

    modified:   index.html

--- js/main.js
+++ js/main.js
@@ -114,3 +114,43 @@ $(window).scroll(function () {
     $(".navbar").removeClass("active");
   }
 });
+
+//scroll trigger
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#main",
+    start: "50% 50%",
+    end: "100% 50%",
+    scrub: 2,
+    pin: true,
+  },
+});
+tl.to(
+  "#top",
+  {
+    top: "-50%",
+  },
+  "a"
+)
+  .to(
+    "#bottom",
+    {
+      bottom: "-50%",
+    },
+    "a"
+  )
+  .to(
+    "#top-h",
+    {
+      top: "80%",
+    },
+    "a"
+  )
+  .to(
+    "#bottom-h",
+    {
+      bottom: "-80%",
+    },
+    "a"
+  );
